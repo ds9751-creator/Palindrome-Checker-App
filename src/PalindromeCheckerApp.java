9c7be8e5cd67f9d0a873445da4241db4bd7803d6
ds9751-creator <ds9751@srmist.edu.in>
2026-02-27 15:42:00 +0530

    Hardcoded result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,12 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         String original = "madam";
-        String reversed = ""; // This starts empty
-
-        // 2. The For Loop
-        // int i = original.length() - 1  -> Start at the last character index
-        // i >= 0                        -> Keep going until we hit the first character
-        // i--                           -> Move one step backward each time
+        String reversed = "";
         for (int i = original.length() - 1; i >= 0; i--) {
             reversed = reversed + original.charAt(i);
         }
