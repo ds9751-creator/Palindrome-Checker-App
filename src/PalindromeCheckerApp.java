65174da035a01b6a99d468ce5e8c4067a612de87
ds9751-creator <ds9751@srmist.edu.in>
2026-02-27 16:07:29 +0530

    UC5

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,22 @@
+import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+        String original = "radar";
+        Stack<Character> stack = new Stack<>();
 
+        for (int i = 0; i < original.length(); i++) {
+            stack.push(original.charAt(i));
+        }
+
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is NOT a palindrome.");
+        }
     }
 }
