daa0217e67d0c2144e752fe72aca5f4901d16974
ds9751-creator <ds9751@srmist.edu.in>
2026-02-27 16:09:33 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+        String original = "racecar";
 
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        for (int i = 0; i < original.length(); i++) {
+            char c = original.charAt(i);
+            stack.push(c);
+            queue.add(c);
+        }
+
+        boolean isPalindrome = true;
+        while (!stack.isEmpty()) {
+            if (!stack.pop().equals(queue.remove())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is NOT a palindrome.");
+        }
     }
 }
