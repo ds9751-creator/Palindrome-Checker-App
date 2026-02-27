a06f3750f7d1197717536b0f614c6a86646d9f8a
ds9751-creator <ds9751@srmist.edu.in>
2026-02-27 15:11:51 +0530

    Completed basic palindrome logic

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Verion : 1.0");
+        System.out.println("System Initialized Successfully");
     }
 }
