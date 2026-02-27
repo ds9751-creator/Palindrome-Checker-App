1c6c747875bb27d7bd21d8f6509fe18212191f8c
ds9751-creator <ds9751@srmist.edu.in>
2026-02-27 16:01:38 +0530

    UC4

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,21 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-
+        String original = "level";
+        char[] charArray = original.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+        boolean isPalindrome = true;
+        while (start < end) {
+            if (charArray[start] != charArray[end]) {
+                isPalindrome = false;
+            }
+            start++;
+            end--;
+        }
+        if (isPalindrome) {
+            System.out.println("'" + original + "' is a palindrome.");
+        } else {
+            System.out.println("'" + original + "' is NOT a palindrome.");
+        }
     }
 }
