87f6d95be712f6f308c9d925705514844c9682c1
ds9751-creator <ds9751@srmist.edu.in>
2026-02-27 16:11:33 +0530

    UC7

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,29 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+        String original = "deified";
+        Deque<Character> deque = new ArrayDeque<>();
 
+        for (int i = 0; i < original.length(); i++) {
+            deque.addLast(original.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is NOT a palindrome.");
+        }
     }
 }
