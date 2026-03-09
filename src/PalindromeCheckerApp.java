f689d8e513c176e29c682b3e1a191a2492f82890
ds9751-creator <ds9751@srmist.edu.in>
2026-03-09 14:57:42 +0530

    UC10

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,31 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class PalindromeCheckerApp {
+
+    public static boolean isPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string to check if it is a palindrome:");
+        String input = scanner.nextLine().replaceAll("\\s+", "").toLowerCase();
+
+        if (isPalindrome(input)) {
+            System.out.println("The string is a palindrome (case-insensitive & ignoring spaces).");
+        } else {
+            System.out.println("The string is NOT a palindrome.");
+        }
+
+        scanner.close();
     }
 }
