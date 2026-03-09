f0236c65f66bde8592231c7d0fbca62568029671
ds9751-creator <ds9751@srmist.edu.in>
2026-03-09 14:59:21 +0530

    UC11

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
-public class PalindromeCheckerApp {
+class PalindromeChecker {
+    private String text;
 
-    public static boolean isPalindrome(String str) {
+    public PalindromeChecker(String text) {
+        this.text = text.replaceAll("\\s+", "").toLowerCase();
+    }
+
+    public boolean checkPalindrome() {
         int start = 0;
-        int end = str.length() - 1;
+        int end = text.length() - 1;
         while (start < end) {
-            if (str.charAt(start) != str.charAt(end)) {
+            if (text.charAt(start) != text.charAt(end)) {
                 return false;
             }
             start++;
@@ -14,14 +19,17 @@ public class PalindromeCheckerApp {
         }
         return true;
     }
+}
 
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string to check if it is a palindrome:");
-        String input = scanner.nextLine().replaceAll("\\s+", "").toLowerCase();
+        String input = scanner.nextLine();
 
-        if (isPalindrome(input)) {
-            System.out.println("The string is a palindrome (case-insensitive & ignoring spaces).");
+        PalindromeChecker checker = new PalindromeChecker(input);
+        if (checker.checkPalindrome()) {
+            System.out.println("The string is a palindrome.");
         } else {
             System.out.println("The string is NOT a palindrome.");
         }
