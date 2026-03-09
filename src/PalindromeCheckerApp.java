fb565b4e57c4becef8baef85bc2e987c075dcd9c
ds9751-creator <ds9751@srmist.edu.in>
2026-03-09 14:53:28 +0530

    ADD

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,27 +2,7 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    public static boolean isPalindrome(String str, int start, int end) {
-        if (start >= end) {
-            return true;
-        }
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-        return isPalindrome(str, start + 1, end - 1);
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string to check if it is a palindrome:");
-        String input = scanner.nextLine().replaceAll("\\s+", "").toLowerCase();
-
-        if (isPalindrome(input, 0, input.length() - 1)) {
-            System.out.println("The string \"" + input + "\" is a palindrome.");
-        } else {
-            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
-        }
-
-        scanner.close();
+    public static void main(String[] args){
+        
     }
 }
