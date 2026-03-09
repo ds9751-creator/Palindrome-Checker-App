8b19187ee063d9316707117f95658bcd9f0d3c4f
ds9751-creator <ds9751@srmist.edu.in>
2026-03-09 14:45:44 +0530

    UC9

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,87 +1,23 @@
 import java.util.Scanner;
 
-class Node {
-    char data;
-    Node next;
+public class PalindromeCheckerApp {
 
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class SinglyLinkedList {
-    Node head;
-
-    void add(char data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-        } else {
-            Node current = head;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    boolean isPalindrome() {
-        if (head == null || head.next == null) {
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
             return true;
         }
-
-        Node slow = head;
-        Node fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
         }
-
-        Node secondHalfStart = reverseList(slow.next);
-
-        Node firstHalf = head;
-        Node secondHalf = secondHalfStart;
-        boolean palindrome = true;
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                palindrome = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        slow.next = reverseList(secondHalfStart);
-
-        return palindrome;
+        return isPalindrome(str, start + 1, end - 1);
     }
 
-    private Node reverseList(Node head) {
-        Node prev = null;
-        Node current = head;
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-        return prev;
-    }
-}
-
-public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string to check if it is a palindrome:");
         String input = scanner.nextLine().replaceAll("\\s+", "").toLowerCase();
 
-        SinglyLinkedList list = new SinglyLinkedList();
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
-
-        if (list.isPalindrome()) {
+        if (isPalindrome(input, 0, input.length() - 1)) {
             System.out.println("The string \"" + input + "\" is a palindrome.");
         } else {
             System.out.println("The string \"" + input + "\" is NOT a palindrome.");
